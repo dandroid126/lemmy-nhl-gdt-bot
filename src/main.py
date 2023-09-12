def9bde9dc5dca89620f912d2204ae3683ebee0e
dandroid <mail@dandroid.app>
2023-09-12 18:47:13 -0500

    Now runs in a loop until is_interrupted is set to true. Processes all games from two day span once it is one hour before the game starts

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import time
 
 from src.utils import nhl_api_client, constants, post_utils, db_client, datetime_utils
 from src.utils.lemmy_client import LemmyClient
@@ -8,6 +9,8 @@ global bot_name
 global password
 global lemmy_instance
 global community_name
+
+DELAY_BETWEEN_UPDATING_POSTS = 60
 
 
 def get_env_variables():
@@ -28,15 +31,18 @@ get_env_variables()
 db_client.initialize(constants.DB_PATH)
 # TODO:
 #  change this to current day
-#  loop all games in day
 #  With the current implementation games will be updated for two days before they are no longer updated.
 #   This will cover if a game goes past midnight, but it might be excessive. REVISIT THIS LATER.
-games = nhl_api_client.get_games('2022-10-31', '2022-11-1')
-print(games)
 lemmy_client = LemmyClient(lemmy_instance, bot_name, password, community_name)
-post_id = db_client.get_post_id(games[0].id)
-if datetime_utils.is_time_to_make_post(datetime_utils.get_current_time_as_utc(), games[0].start_time):
-    if post_id is not None:
-        lemmy_client.update_post(post_utils.get_title(games[0]), post_utils.get_body(games[0]), post_id)
-    else:
-        lemmy_client.create_post(post_utils.get_title(games[0]), post_utils.get_body(games[0]), games[0].id)
+
+is_interrupted = False
+while not is_interrupted:
+    games = nhl_api_client.get_games('2022-10-31', '2022-11-1')
+    for game in games:
+        post_id = db_client.get_post_id(game.id)
+        if datetime_utils.is_time_to_make_post(datetime_utils.get_current_time_as_utc(), game.start_time):
+            if post_id is not None:
+                lemmy_client.update_post(post_utils.get_title(game), post_utils.get_body(game), post_id)
+            else:
+                lemmy_client.create_post(post_utils.get_title(game), post_utils.get_body(game), game.id)
+    time.sleep(DELAY_BETWEEN_UPDATING_POSTS)
